93dbfb99e5d61d99f93c7a25176603c295d3e629
SePe19 <seba217i@stud.kea.dk>
2023-05-27 15:07:38 +0200

    testing

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def _(filename):
 @get("/")
 def index():
     try:
-        print("Maja howdy")
+        print("freddy boi")
         db = dbconnection.db()
         tweets = db.execute("SELECT * FROM tweets").fetchall()
         trends = db.execute("SELECT * FROM trends").fetchall()
