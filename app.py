d243157baa6d528b769140f2b2de5a84f533a957
SePe19 <seba217i@stud.kea.dk>
2023-05-29 01:14:03 +0200

    testing

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def _(filename):
 @get("/")
 def index():
     try:
-        # print("webhook trash")
+        print("webhook trash")
         db = dbconnection.db()
         tweets = db.execute("SELECT * FROM tweets").fetchall()
         trends = db.execute("SELECT * FROM trends").fetchall()
