895d579bc988f07ad7ac6dfbdb137aaef791162d
SePe19 <seba217i@stud.kea.dk>
2023-05-29 00:05:31 +0200

    testing

--- js/like.js
+++ js/like.js
@@ -16,6 +16,7 @@ async function getLikeCount() {
     try {
         const tweet_id = document.getElementById("tweet_id_likes").value
         console.log("YUUYYUUYUYUYUY",tweet_id)
+        console.log("YUUYYUUYUYUYUY",tweet_id)
         const response = await fetch(`/${tweet_id}/likes`);
         const clonedResponse = response.clone();
 
