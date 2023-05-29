f67f81ab9795fac1749c1128dca926179e5d0bda
SePe19 <seba217i@stud.kea.dk>
2023-05-30 01:35:16 +0200

    testing

--- app.py
+++ app.py
@@ -42,7 +42,8 @@ def index():
         trends = db.execute("SELECT * FROM trends").fetchall()
         users = db.execute("SELECT * FROM users").fetchall()
         likes = db.execute("SELECT * FROM likes").fetchall()
-        print("LIKESHERE", likes)
+        if likes:
+            print("LIKESHERE", likes)
         user_cookie = dbconnection.user()
         users_and_tweets = db.execute("SELECT * FROM users_and_tweets ORDER BY tweet_created_at DESC").fetchall()
         if user_cookie is None:
