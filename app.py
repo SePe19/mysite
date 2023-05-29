3e35c42243b4a7470f8ffa000d36b99c808b4374
SePe19 <seba217i@stud.kea.dk>
2023-05-30 01:51:03 +0200

    testing

--- app.py
+++ app.py
@@ -51,7 +51,9 @@ def index():
             if user:
                 user.pop("user_password")
                 user_cookie = user
-        return template("index", title="Twitter", tweets=tweets, trends=trends, users=users, users_and_tweets=users_and_tweets, user_cookie=user_cookie)
+        users = db.execute("SELECT * FROM users WHERE user_id !=?", (user_cookie["user_id"],)).fetchall()
+        following = db.execute("SELECT followee_id FROM followers WHERE follower_id = ?", (user_cookie["user_id"],)).fetchall()
+        return template("index", title="Twitter", tweets=tweets, trends=trends, users=users, users_and_tweets=users_and_tweets, following=following, user_cookie=user_cookie)
     except Exception as ex:
         print("fejl",ex)
         response.status = 400
