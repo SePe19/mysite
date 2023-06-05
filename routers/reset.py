dd8b1178e2312645ab5b3be7a8211df5f972fcf0
SePe19 <seba217i@stud.kea.dk>
2023-06-05 21:09:38 +0200

    testing

--- routers/reset.py
+++ routers/reset.py
@@ -18,10 +18,8 @@ def send_reset_email():
         user_password = bcrypt.hashpw(dbconnection.validate_password().encode("utf-8"), salt)
         user = db.execute("SELECT * FROM users WHERE user_email = ? LIMIT 1", (user_email,)).fetchone()
         if user:
-            print("HELLO FROM THE OTHER SIDE3")
             user_active = 0
-            user_deactive = db.execute("UPDATE users SET user_password = ?, user_active = ? WHERE user_email = ?", (user_password, user_active, user_email)).rowcount
-            print("DANGERDANGER",user_deactive)
+            db.execute("UPDATE users SET user_password = ?, user_active = ? WHERE user_email = ?", (user_password, user_active, user_email)).rowcount
             db.commit()
             email_verification(user_email, user["user_name"])
         return {"info reset":"Succesfully sent reset password email"}
