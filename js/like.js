88ef08db12434866f96decc4bca9df52aa735831
SePe19 <seba217i@stud.kea.dk>
2023-06-06 14:24:45 +0200

    testing

--- js/like.js
+++ js/like.js
@@ -17,7 +17,6 @@ async function like(element) {
     likesCountElement.innerHTML = parseTwitterNumber(likesCount)
     const likeButtons = document.getElementsByClassName("likes")
     const likedTweet = document.querySelector(".liked-tweet")
-    const notLikedTweet = document.querySelector(".not-liked-tweet")
     for (let i = 0; i < likeButtons.length; i++) {
         likeButtons[i].onclick = function() {
             console.log("THIS", element)
@@ -27,7 +26,35 @@ async function like(element) {
             like(element)
             likedTweet.classList.remove("liked-tweet")
             likedTweet.classList.add("not-liked-tweet")
-        } else {
+        }
+    }
+}
+
+async function unlike(element) {
+    console.log("ELEMENT", element);
+    console.log("ELEMENT type", typeof element);
+    console.log("ELEMENT instanceof Element", element instanceof Element);
+    console.log("ELEMENT instanceof HTMLElement", element instanceof HTMLElement);
+    let tweet_id = element.querySelector("input[name='tweet_id_likes']").value
+    console.log("VALUE", tweet_id)
+    const formData = new FormData()
+    formData.append('tweet_id', tweet_id)
+    const response = await fetch("/like", {
+        method: "POST",
+        body: formData
+    })
+    const data = await response.json()
+    const likesCountElement = document.querySelector(".like-count-" + tweet_id)
+    const likesCount = await getLikeCount(tweet_id)
+    likesCountElement.innerHTML = parseTwitterNumber(likesCount)
+    const likeButtons = document.getElementsByClassName("likes")
+    const notLikedTweet = document.querySelector(".not-liked-tweet")
+    for (let i = 0; i < likeButtons.length; i++) {
+        likeButtons[i].onclick = function() {
+            console.log("THIS", element)
+            like(element)
+        }
+        if (!likeButtons[i].querySelector(".liked-tweet").value) {
             like(element)
             notLikedTweet.classList.remove("not-liked-tweet")
             notLikedTweet.classList.add("liked-tweet")
