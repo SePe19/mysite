7f3168e5ffa901482a857fd8bc83e219b1f57a18
SePe19 <seba217i@stud.kea.dk>
2023-06-06 11:22:40 +0200

    testing

--- app.py
+++ app.py
@@ -54,6 +54,10 @@ def index():
                 user_cookie = user
         likes = db.execute("SELECT * FROM likes WHERE likes_user_fk = ?", (user_cookie["user_id"])).fetchall()
         print("LIKESHERE", likes)
+        likes = {
+            "likes_user_fk" : likes["likes_user_fk"],
+            "likes_tweet_fk" : likes["likes_tweet_fk"]
+        }
         following = db.execute("SELECT followee_id FROM followers WHERE follower_id = ?", (user_cookie["user_id"],)).fetchall()
         return template("index", title="Twitter", tweets=tweets, trends=trends, users=users, users_and_tweets=users_and_tweets, following=following, user_cookie=user_cookie, likes=likes)
     except Exception as ex:
