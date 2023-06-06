2a2d1290175f0e5bb579baeb57069e22f295d738
SePe19 <seba217i@stud.kea.dk>
2023-06-06 11:38:55 +0200

    testing

--- app.py
+++ app.py
@@ -53,8 +53,14 @@ def index():
                 user.pop("user_password")
                 user_cookie = user
         likes = db.execute("SELECT * FROM likes WHERE likes_user_fk = ?", (user_cookie["user_id"],)).fetchall()
+        likes_dict = []
+        for like in likes:
+            likes_dict.append({
+                "likes_user_fk": like["likes_user_fk"],
+                "likes_tweet_fk": like["likes_tweet_fk"]
+            })
         following = db.execute("SELECT followee_id FROM followers WHERE follower_id = ?", (user_cookie["user_id"],)).fetchall()
-        return template("index", title="Twitter", tweets=tweets, trends=trends, users=users, users_and_tweets=users_and_tweets, following=following, user_cookie=user_cookie, likes=likes)
+        return template("index", title="Twitter", tweets=tweets, trends=trends, users=users, users_and_tweets=users_and_tweets, following=following, user_cookie=user_cookie, likes=likes_dict)
     except Exception as ex:
         print("fejl",ex)
         response.status = 400
