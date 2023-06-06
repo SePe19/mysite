1e042a395115b3f2f01244a87cf8187aa76182b3
SePe19 <seba217i@stud.kea.dk>
2023-06-06 22:32:05 +0200

    testing

--- js/like.js
+++ js/like.js
@@ -1,5 +1,4 @@
 async function like(element) {
-    const user_id = element.querySelector("input[name='user_id']").value
     const tweet_id = element.querySelector("input[name='tweet_id_likes']").value
     const formData = new FormData()
     formData.append('tweet_id', tweet_id)
@@ -15,18 +14,15 @@ async function like(element) {
     likesElement.onclick = function() {
         unlike(element)
     }
-    console.log("LIKE CLASSLIST", likesElement.classList)
     
     const likesCountElement = document.querySelector(".like-count-" + tweet_id)
-    likesCountElement.classList.remove("liked-tweet")
-    likesCountElement.classList.add("not-liked-tweet")
+    likesCountElement.classList.remove("not-liked-tweet")
+    likesCountElement.classList.add("liked-tweet")
     const likesCount = await getLikeCount(tweet_id)
     likesCountElement.innerHTML = parseTwitterNumber(likesCount)
-    console.log("LIKE COUNT CLASSLIST", likesCountElement.classList)
 }
 
 async function unlike(element) {
-    const user_id = element.querySelector("input[name='user_id']").value
     const tweet_id = element.querySelector("input[name='tweet_id_likes']").value
     const formData = new FormData()
     formData.append('tweet_id', tweet_id)
@@ -42,14 +38,12 @@ async function unlike(element) {
     likesElement.onclick = function() {
         like(element)
     }
-    console.log("UNLIKE CLASSLIST", likesElement.classList)
     
     const likesCountElement = document.querySelector(".like-count-" + tweet_id)
-    likesCountElement.classList.remove("not-liked-tweet")
-    likesCountElement.classList.add("liked-tweet")
+    likesCountElement.classList.remove("liked-tweet")
+    likesCountElement.classList.add("not-liked-tweet")
     const likesCount = await getLikeCount(tweet_id)
     likesCountElement.innerHTML = parseTwitterNumber(likesCount)
-    console.log("UNLIKE COUNT CLASSLIST", likesCountElement.classList)
 }
 
 async function getLikeCount(tweet_id) {
