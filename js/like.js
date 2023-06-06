4bd090572711efc51508424701843092832b36d1
SePe19 <seba217i@stud.kea.dk>
2023-06-06 10:55:24 +0200

    testing

--- js/like.js
+++ js/like.js
@@ -1,5 +1,5 @@
 async function like(tweet_id) {
-    console.log(tweet_id, tweet_id.value)
+    console.log(tweet_id, tweet_id.input.value)
     tweet_id = tweet_id.value
     const formData = new FormData()
     formData.append('tweet_id', tweet_id)
